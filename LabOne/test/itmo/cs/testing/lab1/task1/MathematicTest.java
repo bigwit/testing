ca3b99989633c949e4d8bb99b49132aff9c48bc3
nikit13 <nikitakur1993@gmail.com>
2014-03-09 14:03:54 -0700

    Добавлен тест для первой задачи. И добавлен pom.xml

---
+++ LabOne/test/itmo/cs/testing/lab1/task1/MathematicTest.java
@@ -0,0 +1,53 @@
+package itmo.cs.testing.lab1.task1;
+
+import static java.lang.Double.*;
+import static java.lang.Math.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MathematicTest {
+
+	private final double argument;
+
+	private final double accuracy;
+
+	private final double expected;
+
+	public MathematicTest(double argument, double accuracy, double expected) {
+		this.argument = argument;
+		this.accuracy = accuracy;
+		this.expected = expected;
+	}
+
+	@Parameters
+	public static List<Object[]> data() {
+		List<Object[]> data = new ArrayList<>();
+
+		data.addAll(Arrays.asList(new Object[][] { { NaN, NaN, NaN },
+				{ NaN, 1e-3, NaN }, { 1.0, NaN, NaN } }));
+
+		for (double acc = 1e-3; acc > 1e-7; acc *= 1e-1) {
+			for (double x = -1; x < 1; x += 1e-1) {
+				data.add(new Object[] { x, acc, atan(x) });
+			}
+		}
+		return data;
+	}
+
+	@Test
+	public void shouldReturnCorrectFunctionResult() {
+		double result = Mathematic.calcArctg(argument, accuracy);
+
+		assertEquals(expected, result, accuracy);
+	}
+
+}
