c6026ac933e69146a43e28f3349c040231cff5e0
Alexey Pismak <apismak@marathonbet.ru>
2014-03-13 03:07:36 +0400

    save

--- LabOne/test/itmo/cs/testing/lab1/task2/BPTreeTest.java
+++ LabOne/test/itmo/cs/testing/lab1/task2/BPTreeTest.java
@@ -1,7 +1,31 @@
 package itmo.cs.testing.lab1.task2;
 
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BPTreeTest {
 	
+	private BPTree<Integer, Integer> bpTree;
 	
+	@Before
+	public void init() {
+		this.bpTree = new BPTree<>();
+	}
+	
+	@Test
+	public void bPlusTreeInsertTest() {
+		for(int i = 0; i < 20; ++i) {
+			Random rnd = new Random();
+			this.bpTree.put(rnd.nextInt(), rnd.nextInt());
+		}
+	}
+	
+	@After
+	public void destroy() {
+		this.bpTree = null;
+	}
 	
 }
