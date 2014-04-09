533b9636fc31159961f54366a2ee653923579787
apismak <apismak@marathonbet.ru>
2014-04-10 01:28:53 +0400

    add parser

---
+++ LabOne/test/itmo/cs/testing/lab2/utils/Parser.java
@@ -0,0 +1,40 @@
+package itmo.cs.testing.lab2.utils;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class Parser {
+
+	private static Logger log = Logger.getLogger(Parser.class.getName());
+
+	private static String DELIMITER = "	";
+
+	public static Collection<ResultUnit> parse(String resource) {
+		Collection<ResultUnit> result = new ArrayList<>();
+		InputStream input = Parser.class.getResourceAsStream(resource);
+		if (input == null) {
+			log.warning(String.format("Resource %s not found", resource));
+			throw new IllegalArgumentException();
+		}
+		Scanner scan = new Scanner(input);
+		while (scan.hasNext()) {
+			result.add(getResultUnit(scan.nextLine()));
+		}
+		return result;
+	}
+
+	private static ResultUnit getResultUnit(String line) {
+		ResultUnit res = new ResultUnit();
+		String[] s = line.split(DELIMITER);
+		try {
+			res.setX(Double.parseDouble(s[0]));
+			res.setY(Double.parseDouble(s[1]));
+		} catch (NumberFormatException | IndexOutOfBoundsException e) {
+			log.warning("Parse exception couse:\n " + e.getStackTrace().toString());
+		}
+		return res;
+	}
+}
---
+++ LabOne/test/itmo/cs/testing/lab2/utils/ResultUnit.java
@@ -0,0 +1,24 @@
+package itmo.cs.testing.lab2.utils;
+
+public class ResultUnit {
+	
+	double x;
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	double y;
+	
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+}
